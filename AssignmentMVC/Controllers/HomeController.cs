581b4a2ccc6a1d9da1c3263cdfde1f18553a937d
Andolo2 <andreasolofsson123@gmail.com>
2023-04-17 15:52:53 +0200

    Add project files.

---
+++ AssignmentMVC/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult HomeIndex()
+        {
+            return View();
+        }
+    }
+}
